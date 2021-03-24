412a7fed165a7ab1fffc113e9a600d6c929b0d9f
Abbos <yulchiyevabbos1@gmail.com>
2021-03-24 22:21:42 +0500

    first commit

---
+++ src/main/java/uz/pdp/apicompany/ApiCompanyApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.apicompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiCompanyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/apicompany/ApiCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.apicompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCompanyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
